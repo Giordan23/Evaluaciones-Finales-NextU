4009475ac04dda86f311349efaa690f3e8bf7b35
Giordan23 <giordan.23w@gmail.com>
2019-03-20 16:02:19 -0500

    Add files via upload

--- JQuery_ProyectoFinal_Giordan_Manrique/js/app1.js
+++ JQuery_ProyectoFinal_Giordan_Manrique/js/app1.js
@@ -1,29 +1,80 @@
+function eliminarEstudiante(codigoEstudiante){
+  localStorage.removeItem(codigoEstudiante)
+  listarEstudiante()
+}
+function editarEstudiante(codigoEstudiante){
+  for (var i = 0; i < localStorage.length; i++) {
+   clave = localStorage.key(i)
+   if (clave==codigoEstudiante) {
+     estudiante = $.parseJSON(localStorage.getItem(clave))
+     $("#codigoEstudiante").val(estudiante.codigo)
+     $("#nombresEstudiante").val(estudiante.nombres)
+     $("#apesEstudiante").val(estudiante.apellidos)
+     $("#notaEstudiante").val(estudiante.nota)
+     $("#obsEstudiante").val(estudiante.observaciones)
+
+   }
+  }
+}
+function listarEstudiante(){
+  var tabla = ""
+  tabla += '<table border=1px>'
+  tabla += '<tr>'
+  tabla += '<th>CODIGO</td>'
+  tabla += '<th>APELLIDOS</td>'
+  tabla += '<th>NOMBRES</td>'
+  tabla += '<th>NOTA</td>'
+  tabla += '<th>OBSERVACIONES</td>'
+  tabla += '<th>EDITAR</td>'
+  tabla += '<th>ELIMINAR</td>'
+  tabla += '</tr>'
+
+  for (var i = 0; i < localStorage.length; i++) {
+    clave = localStorage.key(i)
+    estudiante = $.parseJSON(localStorage.getItem(clave))
+    tabla += '<tr>'
+    tabla += '<td>'+estudiante.codigo+'</td>'
+    tabla += '<td>'+estudiante.apellidos+'</td>'
+    tabla += '<td>'+estudiante.nombres+'</td>'
+    tabla += '<td>'+estudiante.nota+'</td>'
+    tabla += '<td>'+estudiante.observaciones+'</td>'
+    tabla += '<td><button onclick="editarEstudiante(\''+estudiante.codigo+'\');">EDITAR</button></td>'
+    tabla += '<td><button onclick="eliminarEstudiante(\''+estudiante.codigo+'\');">ELIMINAR</button></td>'
+
+    tabla += '</tr>'
+  }
+  tabla += '</table>'
+
+  $("#p1").html(tabla)
+}
+
 $(document).ready(function(){
+
   $("#guardar").on("click",function(){
-    var id = $("#idEstudiante").val()
-    var nombre = $("#nombreEstudiante").val()
-    var apellido = $("#apeEstudiante").val()
+    var codigo = $("#codigoEstudiante").val()
+    var nombres = $("#nombresEstudiante").val()
+    var apellidos = $("#apesEstudiante").val()
+    var nota = $("#notaEstudiante").val()
+    var observaciones = $("#obsEstudiante").val()
 
     var estudiante = {
-      id:id,
-      nombre :nombre,
-      apellido : apellido
+      codigo : codigo,
+      nombres :nombres,
+      apellidos : apellidos,
+      nota : nota,
+      observaciones : observaciones
     }
-    localStorage.setItem(id,JSON.stringify(estudiante))
+    localStorage.setItem(codigo,JSON.stringify(estudiante))
     limpiarCampos()
-    var texto = ""
-    for (var i = 0; i < localStorage.length; i++) {
-      clave = localStorage.key(i)
-      estudiante = $.parseJSON(localStorage.getItem(clave))
-      texto += estudiante.id + " - " + estudiante.nombre +" - "+ estudiante.apellido + "<br>"
-    }
-    $("#p1").html(texto)
-
+    listarEstudiante();
   })
   function limpiarCampos(){
-    $("#idEstudiante").val("")
-    $("#nombreEstudiante").val("")
-    $("#apeEstudiante").val("")
+    $("#codigoEstudiante").val("")
+    $("#nombresEstudiante").val("")
+    $("#apesEstudiante").val("")
+    $("#notaEstudiante").val("")
+    $("#obsEstudiante").val("")
+
   }
 
 })
